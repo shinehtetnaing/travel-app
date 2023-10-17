8e664952fdd112eff50bd918a636d0d05751a24e
ShineHN <shinehtetn.bpit@outlook.com>
2023-10-17 14:01:17 +0630

    button component

---
+++ components/Button.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+type ButtonProps = {
+  type: "button" | "submit";
+  title: string;
+  icon?: string;
+  varient: "btn_dark_green";
+};
+
+const Button = ({ type, title, icon, varient }: ButtonProps) => {
+  return (
+    <button
+      type="button"
+      className={`flexCenter gap-3 rounded-full border ${varient}`}
+    >
+      {icon && <Image src={icon} alt={title} width={24} height={24} />}
+      <label className="bold-16 whitespace-nowrap">{title}</label>
+    </button>
+  );
+};
+
+export default Button;

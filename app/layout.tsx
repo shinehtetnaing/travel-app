4e5f91a271e0e05ac008277e5289292255e821d1
shinehtetnaing <shinehtetnaing1998@gmail.com>
2023-10-31 22:38:53 +0630

    footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar";
 import "./globals.css";
 import type { Metadata } from "next";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Travel",
@@ -16,9 +17,8 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Navbar />
-        <main className="relative overflow-hidden">
-          {children}
-        </main>
+        <main className="relative overflow-hidden">{children}</main>
+        <Footer />
       </body>
     </html>
   );

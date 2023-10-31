34803462ea137f99ba8955dcc2eaf0e2751b52a4
shinehtetnaing <shinehtetnaing1998@gmail.com>
2023-10-31 22:37:49 +0630

    button

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,16 +5,21 @@ type ButtonProps = {
   title: string;
   icon?: string;
   varient: string;
+  full?: boolean;
 };
 
-const Button = ({ type, title, icon, varient }: ButtonProps) => {
+const Button = ({ type, title, icon, varient, full }: ButtonProps) => {
   return (
     <button
-      type="button"
-      className={`flexCenter gap-3 rounded-full border ${varient}`}
+      type={type}
+      className={`flexCenter gap-3 rounded-full border ${varient} ${
+        full && "w-full"
+      }`}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
-      <label className="bold-16 whitespace-nowrap">{title}</label>
+      <label className="bold-16 whitespace-nowrap cursor-pointer">
+        {title}
+      </label>
     </button>
   );
 };
